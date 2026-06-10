01f5087424957584c32ac6810e2a540061890d09
Yitong Dang <tomdang@naivetomcat.cn>
2026-06-10 22:24:26 +0800

    I have no idea

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+set -xe
+
+unset YARN_ENABLE_INTERACTIVE
+
 # Build the project
 yarn docs:build
--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -xe
+
+unset YARN_ENABLE_INTERACTIVE
+
 # Install dependencies
 yarn set version stable
 yarn install
